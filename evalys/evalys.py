d755fcdf42bd93d04eaea39a757aa763ad8918a9
Michael Mercier <michael.mercier@inria.fr>
2016-04-22 18:01:08 +0200

    provide an error message if no display and no ouput provided

--- evalys/evalys.py
+++ evalys/evalys.py
@@ -9,13 +9,15 @@ import argparse
 import os
 import matplotlib
 
+NO_GRAPHICS = False
 # Manage the case where the system as no display
 if not os.environ.get('DISPLAY'):
+    NO_GRAPHICS = True
     matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
-
 from evalys.jobset import JobSet
+
 
 def main():
     parser = argparse.ArgumentParser(description='Generate Gantt charts '
@@ -25,6 +27,9 @@ def main():
                         help='The output Gantt chart file depending on the extension. For example: figure.svg')
 
     args = parser.parse_args()
+    if NO_GRAPHICS and not args.output:
+        print("No available display: please provide an output using the -o,--output option")
+        exit(1)
 
     fig, ax_list = plt.subplots(len(args.inputCSV), sharex=True,
                                 sharey=True)
