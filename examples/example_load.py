0e56240cbc52d85d005aa891a76dad41b3a9f41b
Raphaël Bleuse <raphael.bleuse@imag.fr>
2016-09-19 13:47:38 +0200

    Simplify load visualization example

--- examples/example_load.py
+++ examples/example_load.py
@@ -8,32 +8,32 @@ from evalys.jobset import JobSet
 
 
 def main():
-    # Argument parsing
-    parser = argparse.ArgumentParser(description='Display the load of each processor.')
-    parser.add_argument('jobsCSV',
-                        help='The name of the CSV file which contains jobs information')
-    parser.add_argument('--output', '-o',
-                        help='The output file (format depending on the given extension, pdf is RECOMMENDED). For example: figure.pdf')
-
+    # parse command line
+    parser = argparse.ArgumentParser(
+        description='Display the load of each processor.'
+    )
+    parser.add_argument(
+        'jobsCSV',
+        help='The name of the CSV file which contains jobs information'
+    )
+    parser.add_argument(
+        '--output', '-o',
+        help='The output file (format depending on the given extension, pdf'
+        'is RECOMMENDED). For example: figure.pdf'
+    )
     args = parser.parse_args()
 
-    # Create data structures from input args
-    j = JobSet.from_csv(args.jobsCSV)
+    j = JobSet.from_csv(args.jobsCSV)  # create data structure from input args
+    fig = plt.figure()  # create figure
+    ax = fig.gca()  # extract axes
+    plot_load(j, ax, str(args.jobsCSV))  # build visualization
 
-    # Figure creation
-    nb_subplots = 1
-    fig, ax_list = plt.subplots(nb_subplots, sharex = True, sharey = True)
-    if nb_subplots < 2:
-        ax_list = [ax_list]
-
-    # Plot
-    plot_load(j, ax_list[0], str(args.jobsCSV))
-
-    # Figure outputting
+    # show figure
     if args.output is not None:
         plt.savefig(args.output)
     else:
         plt.show()
+
 
 if __name__ == "__main__":
     main()
