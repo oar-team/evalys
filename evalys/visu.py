65c60ba1a3bc5d00b1d14a7b14c8541864c7cc4b
Millian Poquet <millian.poquet@inria.fr>
2016-09-28 15:31:40 +0200

    Series plot: legend is shown by default

--- evalys/visu.py
+++ evalys/visu.py
@@ -170,8 +170,9 @@ def plot_series(series_type, jobsets, ax_series):
                                             index=indexes[jobset_name])
         # plot series
         for serie_name, serie in series.items():
-            ax_series.plot(serie)
+            ax_series.plot(serie, label=serie_name)
         ax_series.set_title(series_type)
+        ax_series.legend(loc='best')
 
     elif series_type == "all":
         pass
