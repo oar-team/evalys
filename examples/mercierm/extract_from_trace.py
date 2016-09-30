53bdb559dc7d5c84cd26586e3ecbc2ae5e8f0bb1
Michael Mercier <michael.mercier@inria.fr>
2016-09-30 18:33:40 +0200

    improve extract example

--- examples/mercierm/extract_from_trace.py
+++ examples/mercierm/extract_from_trace.py
@@ -11,24 +11,28 @@ write 10 extracted periods by parameter combination to CSV files
 from evalys import workload
 import os
 
-swf_trace = "UniLu-Gaia-2014-2"
 
-results_dir = "./results"
-os.makedirs(results_dir, exist_ok=True)
+def extract_periods(w, swf_trace):
 
-w = workload.Workload.from_csv("../" + swf_trace + ".swf")
+    results_dir = "./results"
+    os.makedirs(results_dir, exist_ok=True)
+    for periods in [12, 60]:
 
-for periods in [12, 60]:
+        for util in range(1, 10):
+            res_table = w.extract_periods_with_given_utilisation(
+                periods, util / 10)
 
-    for util in range(1, 10):
-        res_table = w.extract_periods_with_given_utilisation(
-            periods, util / 10)
+            for results in res_table[:10]:
+                filename = "extracted_{}_{}H_{}util-{}.swf".format(
+                    swf_trace,
+                    periods, util * 10,
+                    results.UnixStartTime)
+                print("Export: {} \n{}".format(filename, results))
+                filepath = results_dir + "/" + filename
+                results.to_csv(filepath)
 
-        for results in res_table[:10]:
-            filename = "extracted_{}_{}H_{}util-{}.swf".format(
-                swf_trace,
-                periods, util * 10,
-                results.UnixStartTime)
-            print("Export: {} \n{}".format(filename, results))
-            filepath = results_dir + "/" + filename
-            results.to_csv(filepath)
+
+if __name__ == "__main__":
+    swf_trace = "UniLu-Gaia-2014-2"
+    w = workload.Workload.from_csv("../" + swf_trace + ".swf")
+    extract_periods(w, swf_trace)
