d2c053c8e3d26122821d9ce12500bf1b975bbfaa
Valentin Reis <valentin.reis@gmail.com>
2016-10-12 14:27:37 +0200

    nix-shell environment for use with a qt5 backend and a pip installation of evalys

---
+++ default.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+rWrapper.override {
+  packages = [
+      python35
+      python35Packages.pyqt5
+      python35Packages.matplotlib
+      python35Packages.pip
+      ];
+}
