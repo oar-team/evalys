57306b85a128ad1c0deda0eba4eaf30638e714c5
Michael Mercier <michael.mercier@inria.fr>
2016-11-10 10:36:47 +0100

    Make fragementation example works for several files

--- examples/mercierm/fragmentation.py
+++ examples/mercierm/fragmentation.py
@@ -1,7 +1,18 @@
+"""
+    Show fragmentation of free slots one one or several jobsets
+
+Usage:
+    fragmentation.py <jobset_csv_files> ... [-h]
+
+Options:
+    -h --help      show this help message and exit.
+"""
+
 from evalys import interval_set
 from evalys.jobset import JobSet
 import numpy as np
 import matplotlib.pyplot as plt
+import docopt
 
 
 def compute_fragmentation(fs):
@@ -11,7 +22,9 @@ def compute_fragmentation(fs):
     area = np.repeat(fs.execution_time, fs["total"])
     area = fs["total"] * fs["execution_time"]
 
-    filtred_e = area[lambda x: x > area.quantile(0.15)][lambda x: x < area.quantile(0.85)]
+    filtred_e = area[
+        lambda x: x > area.quantile(0.15)][
+            lambda x: x < area.quantile(0.85)]
 
     frag = {}
     frag['max'] = 1 - (filtred_e.max() / filtred_e.sum())
@@ -24,18 +37,28 @@ def compute_fragmentation(fs):
     return filtred_e
 
 
-def plot_fragmentation(frag):
-    _, axe = plt.subplots(nrows=2)
-    frag.hist(bins=20, ax=axe[0])
+def plot_fragmentation(frag, axe, label):
+    frag.hist(bins=20, ax=axe[0], alpha=0.5, label=label)
     sq = frag.value_counts()
-    ecdf = sq.sort_index().cumsum()*1./len(sq)
-    ecdf.plot(ax=axe[1])
+    ecdf = sq.sort_index().cumsum()
+    ecdf.plot(ax=axe[1], label=label)
 
+
+def compare_jobsets_fragmentation(files):
+
+    _, axe = plt.subplots(nrows=2)
+
+    for f in files:
+        js = JobSet.from_csv(f)
+        fs = js.free_slots()
+        frag = compute_fragmentation(fs)
+        plot_fragmentation(frag, axe, f.split("/")[-1:][0])
+
+    axe[1].grid(True)
+    plt.legend(loc="lower right")
+    plt.show()
 
 if __name__ == "__main__":
-    js = JobSet.from_csv("~/scratch/small.csv")
-    fs = js.free_slots()
-    frag = compute_fragmentation(fs)
-    plot_fragmentation(frag)
-
-    plt.show()
+    # retrieving arguments
+    arguments = docopt.docopt(__doc__)
+    compare_jobsets_fragmentation(arguments['<jobset_csv_files>'])
