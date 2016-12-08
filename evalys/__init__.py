051d86e12f8b8d396d0be8458417956da79c68a0
Michael Mercier <michael.mercier@inria.fr>
2016-12-08 17:45:00 +0100

    make queue works even if proc_req is not provided

--- evalys/__init__.py
+++ evalys/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.0.0'
+__version__ = '2.0.2'
 VERSION = __version__
--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ def read(fname):
 
 readme = read('README.rst')
 changelog = read('CHANGES.rst').replace('.. :changelog:', '')
-authors = read('AUTHORS.rst')
 
 requirements = [
     'pandas',
