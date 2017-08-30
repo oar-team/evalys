4cc20fe3de45c9c4b8943257148d52331ee4a713
Millian Poquet <millian.poquet@inria.fr>
2017-08-30 13:45:50 +0200

    [misc] add nice color palette from Raphaël

--- evalys/visu.py
+++ evalys/visu.py
@@ -20,6 +20,20 @@ def generate_color_set(nb_colors):
     colors = iter(plt.cm.viridis(np.linspace(0, 1, nb_colors)))
     return list(colors)
 
+def generate_colorblind_friendly_palette():
+    # http://jfly.iam.u-tokyo.ac.jp/color/#pallet
+    cbf_palette = [
+        '#999999',        # grey
+        ( .9,  .6,   0),  # orange
+        (.35,  .7,  .9),  # sky blue
+        (  0,  .6,  .5),  # bluish green
+        (.95,  .9, .25),  # yellow
+        (  0, .45,  .7),  # blue
+        ( .8,  .4,   0),  # vermillion
+        ( .8,  .6,  .7),  # reddish purple
+    ]
+    return cbf_palette
+
 
 def annotate(ax, rect, annot):
     rx, ry = rect.get_xy()
