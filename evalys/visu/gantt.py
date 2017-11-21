b89e0c3cba2d4a6751191f50026d24243dd4f0c1
Raphaël Bleuse <raphael.bleuse@imag.fr>
2017-11-21 15:12:34 +0100

    Refactor visu.gantt.GanttVisualization

--- evalys/visu/gantt.py
+++ evalys/visu/gantt.py
@@ -25,12 +25,21 @@ class GanttVisualization(core.Visualization):
         self.labeler = lambda job: str(job['jobID'])
         self._columns = self.COLUMNS
 
+    def _customize_layout(self):
+        self._ax.grid(True)
+
+        # adapt scale of axes if requested
+        if self.xscale == 'time':
+            self._ax.xaxis.set_major_formatter(
+                matplotlib.dates.DateFormatter('%Y-%m-%d\n%H:%M:%S')
+            )
+
     @staticmethod
-    def adapt_uniq_num(df):
+    def _adapt_uniq_num(df):
         df['uniq_num'] = numpy.arange(0, len(df))
 
     @staticmethod
-    def adapt_time_xscale(df):
+    def _adapt_time_xscale(df):
         # interpret columns with time aware semantics
         df['submission_time'] = pandas.to_datetime(df['submission_time'], unit='s')
         df['starting_time'] = pandas.to_datetime(df['starting_time'], unit='s')
@@ -42,13 +51,13 @@ class GanttVisualization(core.Visualization):
         df['finish_time'] = df['finish_time'].map(matplotlib.dates.date2num)
         df['execution_time'] = df['finish_time'] - df['starting_time']
 
-    def adapt(self, df):
-        self.adapt_uniq_num(df)
+    def _adapt(self, df):
+        self._adapt_uniq_num(df)
         if self.xscale == 'time':
-            self.adapt_time_xscale(df)
+            self._adapt_time_xscale(df)
 
     @staticmethod
-    def annotate(rect, label):
+    def _annotate(rect, label):
         rx, ry = rect.get_xy()
         cx, cy = rx + rect.get_width() / 2.0, ry + rect.get_height() / 2.0
         rect.axes.annotate(
@@ -64,39 +73,38 @@ class GanttVisualization(core.Visualization):
     def round_robin_map(job, palette):
         return palette[job['uniq_num'] % len(palette)]
 
-    def plot_job(self, job):
-        x0 = job['starting_time']
-        duration = job['execution_time']
-        for itv in job['allocated_processors'].intervals():
-            height = itv.sup - itv.inf + 1
-            rect = matplotlib.patches.Rectangle(
-                (x0, itv.inf),
-                duration,
-                height,
-                alpha=self.alpha,
-                facecolor=functools.partial(self.colorer, palette=self.palette)(job),
-                edgecolor='black',
-                linewidth=0.5
-            )
-            self._ax.add_artist(rect)
-            self.annotate(rect, self.labeler(job))
+    def _draw(self, df):
+        def _plot_job(job):
+            x0 = job['starting_time']
+            duration = job['execution_time']
+            for itv in job['allocated_processors'].intervals():
+                height = itv.sup - itv.inf + 1
+                rect = matplotlib.patches.Rectangle(
+                    (x0, itv.inf),
+                    duration,
+                    height,
+                    alpha=self.alpha,
+                    facecolor=functools.partial(self.colorer, palette=self.palette)(job),
+                    edgecolor='black',
+                    linewidth=0.5
+                )
+                self._ax.add_artist(rect)
+                self._annotate(rect, self.labeler(job))
+        #
+        df.apply(_plot_job, axis='columns')
 
     def build(self, jobset):
-        # build the visualization
         df = jobset.df.loc[:, self._columns]  # copy just what is needed
-        self.adapt(df)
-        df.apply(self.plot_job, axis='columns')
+        self._adapt(df)  # extract the data required for the visualization
+        self._customize_layout()  # prepare the layout for displaying the data
+        self._draw(df)  # do the painting job
 
-        # tweak visualization appearance
+        # tweak boundaries to match the studied jobset
         self._ax.set(
             xlim=(df.submission_time.min(), df.finish_time.max()),
             ylim=(jobset.res_bounds.inf - 1, jobset.res_bounds.sup + 2),
         )
-        self._ax.grid(True)
-        if self.xscale == 'time':
-            self._ax.xaxis.set_major_formatter(
-                matplotlib.dates.DateFormatter('%Y-%m-%d\n%H:%M:%S')
-            )
+
 
 
 class DiffGanttVisualization(GanttVisualization):
